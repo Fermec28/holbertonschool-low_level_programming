b233400afc909e1af42a9a437a21b4cdb70a5b9c
Jose Medina <fermed28@gmail.com>
2019-02-06 22:14:49 +0000

    deleting firt char argument on printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	long long int ll_int;
 
-	printf("Size of a %s: %lu byte(s)\n", "char", (unsigned long)sizeof(char));
-	printf("Size of an %s: %lu byte(s)\n", "int", (unsigned long)sizeof(int));
-	printf("Size of a %s: %lu byte(s)\n", "long", (unsigned long)sizeof(long));
-	printf("Size of a %s: %ld byte(s)\n", "long int", sizeof(long int));
-	printf("Size of a %s: %lu byte(s)\n", "long long int", sizeof(ll_int));
-	printf("Size of a %s: %lu byte(s)\n", "float", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(long));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll_int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }

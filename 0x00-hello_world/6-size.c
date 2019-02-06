20e144b05cccb21fccd3994e799a3be2488f4f03
Jose Medina <fermed28@gmail.com>
2019-02-06 23:11:38 +0000

    ading int to print

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(varchar));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(varint));
-	printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(varlong));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(varlong));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(varfloat));
 	return (0);

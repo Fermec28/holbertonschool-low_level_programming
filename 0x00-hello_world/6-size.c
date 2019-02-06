478b5b4c71caaf5f1059bb18ed1929a80e18dc4e
Jose Medina <fermed28@gmail.com>
2019-02-06 22:05:45 +0000

    fixed correct typing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,13 @@
  */
 int main(void)
 {
+	long long int ll_int;
+
 	printf("Size of a %s: %lu byte(s)\n", "char", (unsigned long)sizeof(char));
 	printf("Size of a %s: %lu byte(s)\n", "int", (unsigned long)sizeof(int));
 	printf("Size of a %s: %lu byte(s)\n", "long", (unsigned long)sizeof(long));
-	printf("Size of a %s: %lu byte(s)\n", "long int", (unsigned long)sizeof(long int));
-	printf("Size of a %s: %lu byte(s)\n", "long long int", (unsigned long)sizeof(long long int));
+	printf("Size of a %s: %ld byte(s)\n", "long int", sizeof(long int));
+	printf("Size of a %s: %lu byte(s)\n", "long long int", sizeof(ll_int));
+	printf("Size of a %s: %lu byte(s)\n", "float", sizeof(float));
 	return (0);
 }

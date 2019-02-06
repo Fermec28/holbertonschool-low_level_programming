4b6669aaeb79db0327dcac925b585b0899b8d4be
Jose Medina <fermed28@gmail.com>
2019-02-06 23:08:17 +0000

    adding variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,16 @@
  */
 int main(void)
 {
-	long long int ll_int;
+	char varchar;
+	int varint;
+	long int varlong;
+	long long int l;
+	float varfloat;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
-	printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(long));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll_int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(varchar));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(varint));
+	printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(varlong));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(varfloat));
 	return (0);
 }

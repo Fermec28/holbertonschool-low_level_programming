e7e2f3b2a4d3643b00b9d99c845b1810ca350020
Jose Medina <fermed28@gmail.com>
2019-02-07 20:47:51 +0000

    prit z to a lower case

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Succes)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 122; c >= 97; c--)
+		putchar(c);
+	putchar(10);
+	return (0);
+}

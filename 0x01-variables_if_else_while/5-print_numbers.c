e30aefd10dd97097be552a89efbf415100b09d71
Jose Medina <fermed28@gmail.com>
2019-02-07 20:15:09 +0000

    print digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Succes)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%d",c);
+	}
+	printf("\n");
+	return (0);
+}

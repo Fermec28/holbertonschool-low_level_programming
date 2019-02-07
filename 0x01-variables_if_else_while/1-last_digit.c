4138d6f3b53c174ce659e2e2aab815e5553ea467
Jose Medina <fermed28@gmail.com>
2019-02-07 19:18:47 +0000

    delete worng variable name

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	if (l_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, l_digit);
 	else if (l_digit == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, l_digit);
 	else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l_digit);
 	return (0);

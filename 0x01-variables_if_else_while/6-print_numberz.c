a82cb72f732719c06e25f55b2094aaa46d483ab7
Jose Medina <fermed28@gmail.com>
2019-02-07 20:22:30 +0000

    print digits in char format

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Succes)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}

3b8a0dbaf7ec5d89cb465b759ab03210de4f91fc
Jose Medina <fermed28@gmail.com>
2019-02-07 21:20:54 +0000

    print number with space and comas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ *
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		putchar(c);
+		if (c < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	return (0);
+}

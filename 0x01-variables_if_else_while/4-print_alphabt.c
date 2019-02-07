390a7f9b785ede1c205eb66769cb917bfeac5151
Jose Medina <fermed28@gmail.com>
2019-02-07 19:49:54 +0000

    print with if stament

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Succes)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if ( c != 'q' && c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

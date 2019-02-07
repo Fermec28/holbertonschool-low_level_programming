c713f34e9fe68d3841d8c48e404f71a57e9dbbc8
Jose Medina <fermed28@gmail.com>
2019-02-07 22:24:55 +0000

    print posibility of two digits

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ *
+ */
+int main(void)
+{
+	int digit1;
+	int digit2;
+
+	for (digit1 = 48; digit1 < 58; digit1++)
+	{
+		for (digit2 = 48; digit2 < 58; digit2++)
+		{
+			putchar(digit1);
+			putchar(digit2);
+			if (digit1 != 57 || digit2 !=57)
+			{
+				putchar(32);
+				putchar(44);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}

e9fa72a29d70add39a4b1a8ec5c42a63ef1efd4a
Jose Medina <fermed28@gmail.com>
2019-02-12 05:30:42 +0000

    make sure print 11 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int num;
 	int alpha;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num < 10; num++)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			_putchar(alpha);

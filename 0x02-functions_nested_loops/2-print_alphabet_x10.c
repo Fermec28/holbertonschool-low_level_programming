dd3c7f6e0320dbddaa0e519ae53a3c4b64ed82fa
Jose Medina <fermed28@gmail.com>
2019-02-12 02:04:57 +0000

    add print lower case a-z x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *print_alphabet_x10 - Print alpahbet x10
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int num;
+	int alpha;
+
+	for (num = 0; num <= 10; num++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+		_putchar(10);
+	}
+}

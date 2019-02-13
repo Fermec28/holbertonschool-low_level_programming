01a61a3b4241676e9e1765284b0b59da7102b5f8
Jose Medina <fermed28@gmail.com>
2019-02-13 04:40:05 +0000

    adding betty style

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 /**
+ * main - Entry Point
  *
- *
- *
+ * Return: 0 Always (Sucess)
  */
 int main(void)
 {
-	long int fibonacci=1;
-	int iterator=0;
-	long int previous=0;
+	long int fibonacci = 1;
+	int iterator = 0;
+	long int previous = 0;
 	long int aux;
-	do
-	{
+
+	do {
 		aux = fibonacci;
 		fibonacci += previous;
-		printf("%ld",fibonacci);
+		printf("%ld", fibonacci);
 		previous = aux;
 		iterator++;
 		if (iterator < 50)
 			printf(", ");
-	}
-	while(iterator <50);
+	} while (iterator < 50);
 	printf("\n");
 	return (0);
 }

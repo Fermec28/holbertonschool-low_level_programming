936468ea19439b3344f8d5ff46c346c7593b6649
Jose Medina <fermed28@gmail.com>
2019-02-13 04:35:08 +0000

    calculate fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+int main(void)
+{
+	long int fibonacci=1;
+	int iterator=0;
+	long int previous=0;
+	long int aux;
+	do
+	{
+		aux = fibonacci;
+		fibonacci += previous;
+		printf("%ld",fibonacci);
+		previous = aux;
+		iterator++;
+		if (iterator < 50)
+			printf(", ");
+	}
+	while(iterator <50);
+	printf("\n");
+	return (0);
+}

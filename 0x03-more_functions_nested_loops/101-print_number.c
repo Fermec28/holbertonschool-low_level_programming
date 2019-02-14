e9023b55b560517a48def5c09973fc17d2712154
Jose Medina <fermed28@gmail.com>
2019-02-14 22:51:19 +0000

    solution to print any nnumber

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton"
+/**
+ * print_number - print any number
+ * @n: number to print
+ */
+void print_number(int n)
+{
+	if (n < 10 && n >= 0)
+	{
+		_putchar(n + '0');
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_number(-n);
+	}
+	else
+	{
+		print_number(n / 10);
+		print_number(n % 10);
+	}
+}

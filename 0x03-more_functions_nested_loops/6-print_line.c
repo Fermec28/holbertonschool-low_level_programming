440e73db547d3d5f0d39df88e131b516276d0522
Jose Medina <fermed28@gmail.com>
2019-02-14 19:19:09 +0000

    print line n times

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print-line - print line n times
+ * @n: times to print _
+ */
+void print_line(int n)
+{
+	int iterator;
+	if (n > 0)
+	{
+		for (iterator = 0; iterator < n; iterator++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar(10);
+}

402996f3ecbec2922fa38a1be280f8aecba13ed2
Jose Medina <fermed28@gmail.com>
2019-02-14 22:28:28 +0000

    add betty description and return

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * int _isupper - Function calculate supper
+ *  _isupper - Function calculate supper
  * @c: number to evaluate
  *
+ * Return: 1 if is lower 0 otherwise
  */
 int _isupper(int c)
 {

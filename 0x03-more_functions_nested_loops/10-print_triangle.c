4716a011cd75c82a69ff5109a835c919562490d9
Jose Medina <fermed28@gmail.com>
2019-02-14 21:15:00 +0000

    print triangles

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_square - print # matrix nxn
+ * @size: size of matrix
+ */
+void print_triangle(int size)
+{
+	int row;
+	int column;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < size; column++)
+			{
+				(column + row) >= (size - 1) ? _putchar('#') : _putchar(' ');
+			}
+			_putchar(10);
+		}
+	}
+	else
+		_putchar(10);
+}

39d425caea0a3eb0b5b7515ee0481f912c691df7
Jose Medina <fermed28@gmail.com>
2019-02-14 20:56:24 +0000

    fizz buzz challenge

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Result: 0 Always (Success)
+ */
+int main(void)
+{
+	int iterator;
+
+	for (iterator = 0; iterator <= 100; iterator++)
+	{
+		if (iterator % 3 == 0 && iterator % 5 == 0)
+			printf("FizzBuzz");
+		else if (iterator % 3 == 0)
+			printf("Fizz");
+		else if (iterator % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", iterator);
+		if (iterator != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}

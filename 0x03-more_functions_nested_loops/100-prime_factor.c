8666210789bb7701f7d160b0ca1a5eee8d2ec32c
Jose Medina <fermed28@gmail.com>
2019-02-16 14:10:54 +0000

    solution find prime

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -1,34 +1,64 @@
 #include <stdio.h>
-
-int _isprime(int num);
-
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Return: 0 Alway (Sucess)
+*/
 int main(void)
 {
-	int num = 13;
-	int iterator= 2;
-	int aux = 1;
+	long int num = 612852475143;
+	long int firstprime = 2;
+	long int result;
 
-	printf("%d\n", next_prime(13));
+	result = find_prime(num, firstprime);
+	printf("%ld\n", result);
+	return (0);
 }
+/**
+ * find_prime - find the greater multiply prime
+ * @num: num to calculate the prime
+ * @prime: current prime
+ * Return: the greater multiply prime
+ */
+long int find_prime(long int num, long int prime)
+{
+	long int nextprime = next_prime(prime);
 
-int next_prime (int num)
+	if (_isprime(num))
+		return (num);
+	else if (num % prime == 0)
+		return (find_prime((num / prime), prime));
+	else
+		return (find_prime(num, nextprime));
+}
+/**
+ * next_prime - Calculate the next prime
+ * @num: current prime
+ * Return: the nex prime
+ */
+long int next_prime(long int num)
 {
 	num++;
 	return	(_isprime(num) ? num : next_prime(num));
 }
-int _isprime(int num)
+/**
+ * _isprime - Evaluate if a number is prime
+ * @num: num to evaluate if is prime
+ * Return: 1 if is prime 0 if not
+ */
+long int _isprime(long int num)
 {
-	int divisor =2 ;
+	long int divisor = 2;
 
 	if (num > 7)
 	{
-		while(num % divisor != 0 && divisor < num)
+		while (num % divisor != 0 && divisor < num)
 		{
 			divisor++;
 		}
-		return (divisor == num? 1 : 0);
+		return (divisor == num ? 1 : 0);
 	}
-	else if (num == 1 || num == 2 || num == 3 || num == 5 || num ==7)
+	else if (num == 1 || num == 2 || num == 3 || num == 5 || num == 7)
 		return (1);
 	else
 		return (0);

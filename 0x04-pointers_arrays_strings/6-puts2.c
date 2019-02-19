221fa13f44e548456e9f827eec0c98ff01cd6217
Jose Medina <fermed28@gmail.com>
2019-02-19 19:03:42 +0000

    check previous direction

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,7 @@ void puts2(char *str)
 {
 	int cont = 0;
 
-	while (*(str + cont) && *(str + cont) != '\0')
+	while (*(str + cont) && (*(str + cont) != '\0' || *(str + cont - 1) != '\0')
 	{
 		_putchar(*(str + cont));
 		cont += 2;

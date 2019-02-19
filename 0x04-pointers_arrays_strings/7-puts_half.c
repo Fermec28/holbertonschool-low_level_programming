d31e9f72616ae112a10626427bac63e9d3de5509
Jose Medina <fermed28@gmail.com>
2019-02-19 04:31:21 +0000

    print half string

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * puts_half - puts half string
+ * @str: string to print
+ */
+void puts_half(char *str)
+{
+	int size;
+
+	size = _strlen(str);
+	size = (size % 2 == 0) ? size / 2 : (size - 1) / 2;
+	while (*(str + size) != '\0')
+	{
+		_putchar(*(str + size));
+		size++;
+	}
+	_putchar(10);
+}
+
+/**
+ * _strlen - Calculate size of string
+ * @s: string to becalculated
+ * Return: size of string
+ */
+int _strlen(char *s)
+{
+	int cont = 0;
+
+	while (*(s + cont) != '\0')
+		cont++;
+	return (cont);
+}

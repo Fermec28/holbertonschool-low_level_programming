f0b75911947965c37eef473deea123f49ca86a51
Jose Medina <fermed28@gmail.com>
2019-02-19 04:45:04 +0000

    print array

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * print_array - print array
+ * @a: array to print
+ * @n: size of array
+ */
+void print_array(int *a, int n)
+{
+	int iterator;
+
+	for (iterator = 0; iterator < n; iterator++)
+	{
+		printf("%d", *(a + iterator));
+		if (iterator != n - 1)
+			printf(" ,");
+	}
+	printf("\n");
+}

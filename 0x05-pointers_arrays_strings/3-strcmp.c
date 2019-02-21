52910ce6bfdc59005e277836d0dbd8204a49a84b
Jose Medina <fermed28@gmail.com>
2019-02-21 18:11:47 +0000

    compare length two strings

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strcmp - compare size
+ * @s1: string 3
+ * @s2: string 2
+ * Return: <1 if s1 <s2  >1 if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int iterator_s1;
+	int iterator_s2;
+
+	for (iterator_s1 = 0; s1[iterator_s1] ; iterator_s1++)
+	{
+	}
+	for (iterator_s2 = 0; s2[iterator_s2] ; iterator_s2++)
+	{
+	}
+	return (iterator_s1 - iterator_s2);
+}

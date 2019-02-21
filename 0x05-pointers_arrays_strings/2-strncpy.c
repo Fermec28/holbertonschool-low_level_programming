27123280b0996d27142af69c474cf47b7f553a60
Jose Medina <fermed28@gmail.com>
2019-02-21 15:57:49 +0000

    solve secod solution

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * *_strncpy - copy string
+ * @dest: destination string
+ * @src: source string
+ * @n: ending point to copy
+ * Return: string concatenated
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int iterator_dest;
+
+	for (iterator_dest = 0; iterator_dest < n; iterator_dest++)
+	{
+		dest[iterator_dest] = src[iterator_dest];
+	}
+	return (dest);
+}

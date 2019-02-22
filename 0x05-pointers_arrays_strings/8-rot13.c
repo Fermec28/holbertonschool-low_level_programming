a07caa59b2d3c1216a2c6237478549b5ce68e781
Jose Medina <fermed28@gmail.com>
2019-02-22 05:13:32 +0000

    root 13

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * *rot13 - change with rot13
+ * @s: string to be manipulated
+ * Return: string manipulated
+ */
+char *rot13(char *s)
+{
+	int iterator;
+	int ikeys = 0;
+	char keys[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char values[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int flag= 1;
+	for (iterator = 0; s[iterator]; iterator++)
+	{
+		while (keys[ikeys] && flag)
+		{
+			if (s[iterator] == keys[ikeys])
+			{
+				s[iterator] = values[ikeys];
+				flag = 0;
+			}
+			ikeys++;
+		}
+		ikeys = 0;
+		flag = 1;
+	}
+
+	return (s);
+}

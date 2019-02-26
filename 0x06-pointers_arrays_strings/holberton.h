b1751e7b827d1d805377cad90e827b49e7956458
Jose Medina <fermed28@gmail.com>
2019-02-26 03:43:28 +0000

    print chessboard

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -5,3 +5,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int is_thesame(char* string, char* substring);
+void print_chessboard(char (*a)[8]);

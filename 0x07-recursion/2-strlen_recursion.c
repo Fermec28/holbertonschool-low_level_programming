79174b759713191b2f37c8d455e17c53383f4e67
Jose Medina <fermed28@gmail.com>
2019-02-27 19:25:10 +0000

    length of string

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to print
+ * Return: Length of array
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}

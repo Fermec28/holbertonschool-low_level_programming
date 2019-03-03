9749c860a10ca60fefbdc0dc9fb153a86f860997
Jose Medina <fermed28@gmail.com>
2019-03-03 14:30:26 +0000

    print name program

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: count of parametters
+ * @argv: Array with the value of parammeters
+ * Return: 0 Always Succes
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}

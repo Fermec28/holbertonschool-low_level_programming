81300e6347600cd2bba1e5f4700f25b0b75071ae
Jose Medina <fermed28@gmail.com>
2019-03-03 16:29:52 +0000

    exchange challenge

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * exchange - calculate minimun count of exhange
+ * @coins: array of coins
+ * @current: represent actual coin
+ * @value: value to be exchange
+ * Return: 1 if can exchange
+ */
+int exchange(int *coins, int current, int value)
+{
+	if (value >= coins[current])
+		return (1 + exchange(coins, current, value - coins[current]));
+	else if (value == 0)
+		return (0);
+	else
+		return (exchange(coins, current + 1, value));
+}
+/**
+ * main - Entry point
+ * @argc: count of parametters
+ * @argv: Array with the value of parammeters
+ * Return: 0 Always Succes
+ */
+int main(int argc, char *argv[])
+{
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (atoi(argv[1]) < 0)
+		printf("0\n");
+	else
+		printf("%d\n", exchange(coins, 0, atoi(argv[1])));
+	return (0);
+}

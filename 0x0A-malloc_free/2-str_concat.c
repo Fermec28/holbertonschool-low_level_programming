7f01ad0bdfc839680330095276dd4279bef40968
Jose Medina <fermed28@gmail.com>
2019-03-05 22:12:27 +0000

    copy two arrays

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdupstr_concat - Duplicate string
+ * @s1: string one
+ * @s2: string two
+ * Return: pointer to copy array
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	unsigned int i;
+	unsigned int size_s1, size_s2;
+
+	for (size_s1 = 0; *(s1 + size_s1) && s1; size_s1++)
+	{
+	}
+	for (size_s2 = 0; *(s2 + size_s2) && s2; size_s2++)
+	{
+	}
+	p =  malloc(sizeof(char) * (size_s1 + size_s2 + 1));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < size_s1 + size_s2; i++)
+	{
+		if (i < size_s1)
+			p[i] = *(s1 + i);
+		else
+			p[i] = *(s2 + i - size_s1);
+	}
+	p[i] = '\0';
+	return (p);
+}

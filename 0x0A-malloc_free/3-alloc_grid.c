5afb3c98928de0c911bc436764a8fd135a8d98d5
Jose Medina <fermed28@gmail.com>
2019-03-06 01:03:16 +0000

    creates an array2D

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - Duplicate string
+ * @width: widht array
+ * @height: height array
+ * Return: pointer to array2D
+ */
+int **alloc_grid(int width, int height)
+{
+	int **p, ip;
+	int ij;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	p = malloc(width * sizeof(int*));
+	if (p == NULL)
+		return (NULL);
+	for (ip = 0; ip < width; ip++)
+	{
+		p[ip] = malloc(sizeof(int) * height);
+		if (p[ip] == NULL)
+			return (NULL);
+		for (ij = 0; ij < height; ij++)
+			p[ip][ij] = 0;
+	}
+	return (p);
+}

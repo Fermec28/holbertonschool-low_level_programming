9a309c9784e415e72649178c9cb8e350b0925e55
Jose Medina <fermed28@gmail.com>
2019-03-06 01:12:05 +0000

    add betty style

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	p = malloc(width * sizeof(int*));
+	p = malloc(width * sizeof(int *));
 	if (p == NULL)
 		return (NULL);
 	for (ip = 0; ip < width; ip++)

ccd0ea7430346d7ad4231ef64503e42448153d00
Jose Medina <fermed28@gmail.com>
2019-03-06 05:27:37 +0000

    print parameters

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -7,8 +7,31 @@
  * @av: height array
  * Return: array
  */
-char *argstostr(int ac, char **av);
+char *argstostr(int ac, char **av)
 {
 	char *p;
+	int i, j, total;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0, total = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++, total++)
+			;
+		total++;
+	}
+	p = malloc((total + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0, total = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++, total++)
+		{
+			p[total] = av[i][j];
+		}
+		p[total] = '\n';
+		total++;
+	}
+	p[total] = '\0';
 	return (p);
 }

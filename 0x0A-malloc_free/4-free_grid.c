6dd84bd9495fa4667eb6544aabffa8be46c3b39a
Jose Medina <fermed28@gmail.com>
2019-03-06 04:27:51 +0000

    mandatory files

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - create grid
+ * @grid: grid
+ * @height: height array
+ */
+void free_grid(int **grid, int height)
+{
+	
+}
---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_gridargstostr - create grid
+ * @ac: grid
+ * @av: height array
+ */
+char *argstostr(int ac, char **av);
+{
+	char *p;
+	return (p);
+}

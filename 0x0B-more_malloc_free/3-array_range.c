4b52dfe10f4f6068a39c9a649c87f4b3ebbd4c84
Jose Medina <fermed28@gmail.com>
2019-03-07 21:40:51 +0000

    build an array

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -1,11 +1,23 @@
 #include "holberton.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * array_range -
- *
- * Return: 
+ * array_range -creates array in range
+ * @min: min number
+ * @max: max number
+ * Return: Pointer
  */
 int *array_range(int min, int max)
 {
+	int *p, i;
 
+	if (min > max)
+		return (NULL);
+	p = malloc(sizeof(int) * (max - min + 1));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = min; i <= max; i++)
+		p[i-min] = i;
+	return (p);
 }

87b7dd7f8931e78134b086581771b39523e5bf8f
Jose Medina <fermed28@gmail.com>
2019-03-07 20:07:03 +0000

    create _calloc

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -2,10 +2,21 @@
 
 /**
  * _calloc -
- *
- * Return: 
+ * @nmemb: number of members
+ * @size: size of each member on bytes
+ * Return: pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *p;
+	int i;
 
+	if (nmemb && size)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		p[i] = 0;
+	return (p);
 }

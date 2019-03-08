ea70fecb2bb8de8934a699c5eadf02cd90e309b6
Jose Medina <fermed28@gmail.com>
2019-03-08 04:20:41 +0000

    add reallox

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -1,11 +1,27 @@
 #include "holberton.h"
 
 /**
- * _realloc -
- *
- * Return: 
+ * _realloc - reallocate memory
+ * @ptr: original pointer
+ * @old_size: old size of pointer
+ * @new_size: new size of pointer
+ * Return: pointer
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	free(ptr);
+	ptr = malloc(new_size);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
 }

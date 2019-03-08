6eedaeec1cde837d4b81f4e0e624c5e96391270b
Jose Medina <fermed28@gmail.com>
2019-03-08 03:38:05 +0000

    bases cases advanced task

---
+++ 0x0B-more_malloc_free/101-mul.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry Point
+ * @argc: Num of arguments
+ * @argb: array of arguments
+ * Return: 0 if its successful
+ */
+int main(int argc, char *argv[])
+{
+	int i_argc, i_argv;
+	if (argc != 3)
+	{
+		exit(98);
+	}
+	for (i_argc = 1; i_argc < i_argc; i++)
+	{
+		for (i_argv = 0; argv[i_argc][i_argv]; i_argv++)
+		{
+			if (isdigit(argv[i_argc][i_argv]))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	return(0);
+}

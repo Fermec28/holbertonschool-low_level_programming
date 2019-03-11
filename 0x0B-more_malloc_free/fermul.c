c38ca36523774fb1f385db954d2048b59dd242a6
Jose Medina <fermed28@gmail.com>
2019-03-11 02:56:39 +0000

    add my print to be suere ready to start the product!

--- 0x0B-more_malloc_free/fermul.c
+++ 0x0B-more_malloc_free/fermul.c
@@ -6,6 +6,8 @@
 void fill_array(char *str, int initial, int final, char value);
 int str_length(char *str);
 void reverse_str(char *str, int size);
+void myprint(char *str);
+void check_digits(int argc, char *argv[]);
 /**
  * main -Entry Point
  * @argc: Num of arguments
@@ -23,21 +25,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	for (i_argc = 1; i_argc < i_argc; i_argc++)
-	{
-		for (i_argv = 0; argv[i_argc][i_argv]; i_argv++)
-		{
-			if (isdigit(argv[i_argc][i_argv]))
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
-	}
+	check_digits(argc, argv);
 	l_str1  = str_length(argv[1]);
 	l_str2  = str_length(argv[2]);
-	reverse_str(argv[1],l_str1);
-	reverse_str(argv[2],l_str2);
+	reverse_str(argv[1],str_length(argv[1]));
+	reverse_str(argv[2],str_length(argv[2]));
 	size = (l_str1 >= l_str2 ? l_str1 : l_str2);
 	p = malloc(sizeof(char) * (size + 1));
 	if (p == NULL)
@@ -45,11 +37,37 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	fill_array(p,0,size - 1, 'A');
+	fill_array(p,0,size - 1, '0');
 	p[size] = '\0';
-	printf("New array is %s\n",p);
 	free(p);
 	return (0);
+}
+
+void check_digits(int argc, char *argv[])
+{
+	int i_argc, i_argv;
+
+	for (i_argc = 1; i_argc < argc; i_argc++)
+	{
+		for (i_argv = 0; argv[i_argc][i_argv]; i_argv++)
+		{
+			if (!isdigit(argv[i_argc][i_argv]))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+}
+void myprint(char *str)
+{
+	int i = 0;
+	while(str[i])
+	{
+		printf("%c", str[i]);
+		i++;
+	}
+printf("\n");
 }
 void reverse_str(char *str, int size)
 {

a760254349680f272ed02f5dcffc64aad123de73
Jose Medina <fermed28@gmail.com>
2019-03-11 02:30:02 +0000

    create reverse function

--- 0x0B-more_malloc_free/fermul.c
+++ 0x0B-more_malloc_free/fermul.c
@@ -4,7 +4,8 @@
 #include <ctype.h>
 
 void fill_array(char *str, int initial, int final, char value);
-
+int str_length(char *str);
+void reverse_str(char *str, int size);
 /**
  * main -Entry Point
  * @argc: Num of arguments
@@ -14,6 +15,8 @@ void fill_array(char *str, int initial, int final, char value);
 int main(int argc, char *argv[])
 {
 	int i_argc, i_argv;
+	int l_str1, l_str2, size;
+	char *p;
 
 	if (argc != 3)
 	{
@@ -31,18 +34,45 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
+	l_str1  = str_length(argv[1]);
+	l_str2  = str_length(argv[2]);
+	reverse_str(argv[1],l_str1);
+	reverse_str(argv[2],l_str2);
+	size = (l_str1 >= l_str2 ? l_str1 : l_str2);
+	p = malloc(sizeof(char) * (size + 1));
+	if (p == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	fill_array(p,0,size - 1, 'A');
+	p[size] = '\0';
+	printf("New array is %s\n",p);
+	free(p);
 	return (0);
 }
+void reverse_str(char *str, int size)
+{
+	int length;
+	int iterator;
+	char aux;
 
+	length = size % 2 == 0 ? size / 2 : ((size / 2) + 1);
+	for  (iterator = 0; iterator < length; iterator++)
+	{
+		aux = str[iterator];
+		str[iterator] = str[size - iterator -1];
+		str[size - iterator - 1] = aux;
+	}
+
+}
 void fill_array(char *str, int initial, int final, char value)
 {
 	int iterator;
 	for(iterator = initial; iterator <= final; iterator++)
 		str[iterator] = value;
-
-
 }
-int length(char *str)
+int str_length(char *str)
 {
 	int iterator =0;
 

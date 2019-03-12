3b43935bd29b160b6e3bfa7505783dab128e6a57
Jose Medina <fermed28@gmail.com>
2019-03-12 03:38:43 +0000

    add _strcpy and bases cases

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,13 +1,39 @@
 #include <stdlib.h>
 #include "dog.h"
+
+char *_strcpy(char *str)
+{
+	char *p;
+	int size;
+
+	for (size = 0; *(str + size); size++)
+		;
+	p = malloc(sizeof(char) * (size + 1));
+	if (!p)
+		return NULL;
+	for (; size >= 0; size--)
+		p[size] = str[size];
+	return (p);
+}
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t * p;
 	p = malloc(sizeof(dog_t));
 	if (p == NULL)
-		return NULL;
-	p->name = name;
+		return (NULL);
+	p->name = _strcpy(name);
+	if (p->name == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
 	p->age = age;
-	p->owner = owner;
+	p->owner = _strcpy(owner);
+	if (p->owner == NULL)
+	{
+		free(p->name);
+		free(p);
+		return (NULL);
+	}
 	return (p);
 }

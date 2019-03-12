bba747047785fba57f700cec9c94a424f64e855e
Jose Medina <fermed28@gmail.com>
2019-03-12 00:15:16 +0000

    print an structure

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -1,11 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * new_dog -
- *
- * Return: 
+ * print_dog - Print simple dog
+ * @d: pointer to struct
  */
-dog_t *new_dog(char *name, float age, char *owner)
+void print_dog(struct dog *d)
 {
-
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name == NULL) ? "nil" : d->name);
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", (d->owner == NULL) ? "nil" : d->owner);
+	}
 }

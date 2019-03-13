4d1f89811d18b07197cf60d683ea233052d15c6a
Jose Medina <fermed28@gmail.com>
2019-03-13 20:44:43 +0000

    add solution 3 task

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,11 +1,22 @@
 #include "function_pointers.h"
 
 /**
- * int_index -
- *
- * Return: 
+ * int_index - return index
+ * @array: array of integer
+ * @size: size of array
+ * @cmp: function to compare
+ * Return: -1 no matches or size <= 0,index
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
 
+	if (array == NULL || cpm == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
 }

830234d854bb94c9770c8e6cd10e02b936135a63
Jose Medina <fermed28@gmail.com>
2019-03-13 20:26:49 +0000

    aply function to each element of array

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,11 +1,18 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator -
- *
- * Return: 
- */
+ * array_iterator - aplies function to each array element
+ * @array: array
+ * @size: size
+ * @action: function
+1 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
 
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }

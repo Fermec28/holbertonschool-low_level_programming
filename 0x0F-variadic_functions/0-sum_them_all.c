71174a211a9ca0b0b7a32166dcdbf334fdec73c2
Jose Medina <fermed28@gmail.com>
2019-03-15 19:14:16 +0000

    add betty style and correct .h

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -7,13 +7,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0, i;
+	unsigned int sum = 0, i;
 	va_list valist;
 
 	if (n <= 0)
 		return (sum);
 	va_start(valist, n);
-	for (i = 0; i < n; i++ )
+	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
 	return (sum);
 }

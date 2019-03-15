119822e7c5dd6d987f8d9531f6ddc77849511e0d
Jose Medina <fermed28@gmail.com>
2019-03-15 19:35:32 +0000

    number separator

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -1,11 +1,22 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers -
- *
- * Return: 
+ * print_numbers - print n numbers
+ * @separator: separator each number
+ * @n:  number of arguments
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int iterator;
+	va_list va;
 
+	if (!(separator == NULL || n <= 0))
+	{
+		va_start(va, n);
+		for (iterator = 0; iterator < n - 1; iterator++)
+		{
+			printf("%d%s", va_arg(va, int), separator);
+		}
+		printf("%d\n", va_arg(va, int));
+	}
 }

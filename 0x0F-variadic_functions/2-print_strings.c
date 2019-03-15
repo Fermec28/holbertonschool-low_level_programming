e1e2b01d1bc968f4d8a5a7643fb695245fcf7e41
Jose Medina <fermed28@gmail.com>
2019-03-15 19:45:07 +0000

    print strings

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -1,11 +1,23 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings -
- *
- * Return: 
+ * print_strings - print multiple strings
+ * @separator: separator
+ * @n: numnber of elements
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int iterator;
+	va_list valist;
+
+	if (!(separator == NULL || n <= 0))
+	{
+		va_start(valist, n);
+		for (iterator = 0; iterator < n - 1; iterator++)
+		{
+			printf("%s%s", va_arg(valist, char*), separator);
+		}
+		printf("%s\n", va_arg(valist, char*));
+	}
 
 }

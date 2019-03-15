f2186de4e5ff4abfa4ab0b8744cd26393b133dd3
Jose Medina <fermed28@gmail.com>
2019-03-15 18:02:30 +0000

    solution variadict sum of arguments

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -1,11 +1,19 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all -
- *
- * Return: 
+ * sum_them_all - sum arguments passed
+ * @n: number of parammeters
+ * Return: Sum of arguments
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum = 0, i;
+	va_list valist;
 
+	if (n <= 0)
+		return (sum);
+	va_start(valist, n);
+	for (i = 0; i < n; i++ )
+		sum += va_arg(valist, int);
+	return (sum);
 }
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -3,10 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-int sum_them_all(const unsigned int n, ...)
-void print_numbers(const char *separator, const unsigned int n, ...)
-void print_strings(const char *separator, const unsigned int n, ...)
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...)
 
 #endif /* VARIADIC_FUNCTIONS_H */

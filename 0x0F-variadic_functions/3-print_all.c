644b8921fd9669d308badc6f9d8c68f79d87aac7
Jose Medina <fermed28@gmail.com>
2019-03-17 15:28:45 +0000

    [feature] add structure and header functions

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,11 +1,18 @@
 #include "variadic_functions.h"
-
 /**
- * print_all -
- *
- * Return: 
+ * print_all - print according to format
+ * @format: format to print
  */
 void print_all(const char * const format, ...)
 {
+	int i = 0;
+	if (format != NULL)
+	{
+		while (format[i])
+		{
+			printf("%c\n", format[i]);
+			i++;
+		}
+	}
 
 }
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -8,5 +8,20 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+void print_char(const char *data);
+void print_integer(const char *data);
+void print_float(const char *data);
+void print_string(const char *data);
+/**
+ * struct op - Struct op
+ *
+ * @op: options c: char, i:integer, f:float, s: char
+ * @data: data to print
+ *
+ */
+typedef struct op
+{
+	const char * const op;
+	void (*f)(const char *data);
+} op_t;
 #endif /* VARIADIC_FUNCTIONS_H */

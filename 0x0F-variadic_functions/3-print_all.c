1add6e9bec2aefbc2c3dd098b86bf90b498070f7
Jose Medina <fermed28@gmail.com>
2019-03-17 17:06:38 +0000

    [feature] function that prints depends of format

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -8,28 +8,44 @@ void print_all(const char * const format, ...)
 	int i_format = 0, i_struct = 0;
 	op_t print[] = {
 		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
 		{NULL,NULL}
 	};
 	va_list valist;
 
-	if (format != NULL)
+	va_start(valist, format);
+	while (format != NULL && format[i_format] )
 	{
-		va_start(valist, format);
-		while (format != NULL, format[i_format] )
+		i_struct = 0;
+		while (print[i_struct].op != NULL)
 		{
-			i_struct = 0;
-			while (print[i_struct].op != NULL)
+			if( format[i_format] == *(print[i_struct].op))
 			{
-				if( format[i_format] == *(print[i_struct].op))
-					print[i_struct].f(valist);
-				i_struct++;
+				print[i_struct].f(valist);
+				if (format[i_format + 1] != '\0')
+					printf(", ");
 			}
-			i_format++;
+			i_struct++;
 		}
+		i_format++;
 	}
 	printf("\n");
 }
 void print_char(va_list valist)
 {
 	printf("%c", va_arg(valist, int));
+}
+void print_integer(va_list valist)
+{
+	printf("%d", va_arg(valist, int));
+}
+void print_float(va_list valist)
+{
+	printf("%f", va_arg(valist, double));
+}
+void print_string(va_list valist)
+{
+	printf("%s", va_arg(valist, char*));
 }

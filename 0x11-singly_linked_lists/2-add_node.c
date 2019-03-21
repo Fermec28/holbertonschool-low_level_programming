66e496443c84f7095d4ec47060c59f5708f34e43
Jose Medina <fermed28@gmail.com>
2019-03-21 22:47:12 +0000

    free when str cant copy

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -14,6 +14,11 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	for (; new_node->str[size]; size++)
 		;
 	new_node->str[size] = '\0';

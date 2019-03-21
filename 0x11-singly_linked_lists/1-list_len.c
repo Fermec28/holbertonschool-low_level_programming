2b86a601fef47d269a6cb3a4a4df0228209e1594
Jose Medina <fermed28@gmail.com>
2019-03-21 20:25:12 +0000

    count length of list

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 
 /**
- * list_len -
- *
- * Return: 
+ * list_len - length of list
+ * @h: node
+ * Return: 1 if its not null
  */
 size_t list_len(const list_t *h)
 {
-
+	if (h == NULL)
+		return (0);
+	return (1 + list_len(h->next));
 }

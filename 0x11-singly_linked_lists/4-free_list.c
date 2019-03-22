3e8b756a5a37160f6e1f3448045a03fcf73a9a9a
Jose Medina <fermed28@gmail.com>
2019-03-22 17:54:49 +0000

    free list

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 
 /**
- * free_list -
- *
- * Return: 
+ * free_list - free a list
+ * @head: Head of the list
  */
 void free_list(list_t *head)
 {
-
+	if (head == NULL)
+		;
+	else
+	{
+		free_list(head->next);
+		free(head->str);
+		free(head);
+	}
 }

03b68afd0ce4fe55362db673e7cda86f5ae318a0
Jose Medina <fermed28@gmail.com>
2019-03-25 14:33:41 +0000

    add solution with putchar

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,41 @@
 #include "lists.h"
-
 /**
- * print_listint -
+ * print_number - print any integer number
+ * @n: number
+ */
+void print_number(int number)
+{
+	int aux;
+
+	if (number < 0)
+	{
+		_putchar('-');
+		aux = number / 10;
+		print_number(-aux);
+		aux = number % 10;
+		print_number(-aux);
+	}
+	else
+	{
+		if (number > 9)
+			print_number( number / 10);
+		_putchar((number % 10) + '0');
+	}
+
+}
+/**
+ * print_listint - print all elements in list
  *
- * Return: 
+ * Return: return length of list
  */
 size_t print_listint(const listint_t *h)
 {
-
+	if (h == NULL)
+		return (0);
+	else
+	{
+		print_number(h->n);
+		_putchar('\n');
+		return (1 + print_listint(h->next));
+	}
 }

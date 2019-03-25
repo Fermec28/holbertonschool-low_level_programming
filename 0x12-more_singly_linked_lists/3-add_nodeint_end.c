362ab0e692cfd2d616ef0200909ad28c4cba20ca
Jose Medina <fermed28@gmail.com>
2019-03-25 22:00:14 +0000

    ensure next pointed to NULL

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,6 +21,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	new_node->next = NULL;
 	if(*head == NULL)
 		*head = new_node;
 	else

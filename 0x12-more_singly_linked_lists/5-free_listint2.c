22d5d85895124b197957d42e282385c9b053c0c1
Jose Medina <fermed28@gmail.com>
2019-03-25 22:44:41 +0000

    free with double pointer

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 
 /**
- * free_listint2 -
- *
- * Return: 
+ * free_listint2 - free a list
+ * @head: head of the list
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *aux;
 
+	while(*head != NULL)
+	{
+		aux = (*head)->next;
+		free(*head);
+		*head = aux;
+	}
 }

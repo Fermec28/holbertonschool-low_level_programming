48b31515604d94ea47308b7e669ec1d2438f282a
Jose Medina <fermed28@gmail.com>
2019-03-25 23:37:26 +0000

    [add] solution pop and correct betty issues

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * pop_listint -
- *
- * Return: 
+ * pop_listint - pop element
+ * @head: head of the list
+ * Return: the head nodes data(n)
  */
 int pop_listint(listint_t **head)
 {
+	int value = 0;
+	listint_t *aux;
 
+	if (*head != NULL)
+	{
+		value = (*head)->n;
+		aux = (*head)->next;
+		free(*head);
+		*head = aux;
+	}
+	return (value);
 }

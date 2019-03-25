9a8bae8f2bc151c4cb837d72d6936732778b665d
Jose Medina <fermed28@gmail.com>
2019-03-25 15:30:35 +0000

    fixes betty

--- 0x12-more_singly_linked_lists/2-add_nodeint.c
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -2,7 +2,8 @@
 
 /**
  * add_nodeint - add node at the beggining of the list
- *
+ * @head: head of the list
+ * @n: number to be initilialized the new node
  * Return: adrres of new element or NULL if it failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)

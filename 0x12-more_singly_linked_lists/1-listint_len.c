1699faf290f179b78813c8524ac4e56c112449cb
Jose Medina <fermed28@gmail.com>
2019-03-25 14:44:46 +0000

    calculate length of list

--- 0x12-more_singly_linked_lists/1-listint_len.c
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * listint_len -
- *
- * Return: 
+ * listint_len - calculate length list
+ * @h: node
+ * Return: 1 if its a node
  */
 size_t listint_len(const listint_t *h)
 {
+	if (h == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + listint_len(h->next));
+	}
 
 }

1b28ef86e2ef161366c4cdefe003b5e13fdf57b5
Jose Medina <fermed28@gmail.com>
2019-03-25 23:55:33 +0000

    sum data of list

--- 0x12-more_singly_linked_lists/8-sum_listint.c
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 
 /**
- * sum_listint -
- *
- * Return: 
+ * sum_listint - sum data on list
+ * @head: head of the list
+ * Return: the sum of that data
  */
 int sum_listint(listint_t *head)
 {
-
+	if (head == NULL)
+		return (0);
+	else
+		return (head->n + sum_listint(head->next));
 }

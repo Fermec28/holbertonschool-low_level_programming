6bdf1c93a46fb8b303da2643cda106dc80ebe6bc
Jose Medina <fermed28@gmail.com>
2019-03-26 21:56:48 +0000

    Fixes betty

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,13 +11,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *aux = *head, *new_node;
 	unsigned int iterator = 0;
+
 	do {
 		if (iterator == idx - 1 || (idx == 0 && iterator == 0))
 		{
 			new_node = malloc(sizeof(listint_t));
-			if(!new_node)
+			if (!new_node)
 				return (NULL);
-			if(*head == NULL || idx == 0)
+			if (*head == NULL || idx == 0)
 			{
 				new_node->n = n;
 				new_node->next = *head;
@@ -34,6 +35,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		aux = aux->next;
 		iterator++;
-	}while(aux);
+	} while (aux);
 	return (NULL);
 }

c2b94180b9dc3beb6c0a5f5d8ff20beb3604b4f8
Jose Medina <fermed28@gmail.com>
2019-03-27 03:56:58 +0000

    delete node

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,10 +2,35 @@
 
 /**
  * delete_nodeint_at_index -
- *
- * Return: 
+ * @head: head of list
+ * @index:index to delete
+ * Return: 1 if can removed -1 if not
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *aux = *head, *pivote;
+	unsigned int iterator = 0;
 
+	while(aux)
+	{
+		if (iterator == index - 1 || (index == 0 && iterator == 0))
+		{
+			if(index == 0)
+			{
+				*head = aux->next;
+				free(aux);
+				return (1);
+			}
+			else
+			{
+				pivote = (aux->next)->next;
+				free(aux->next);
+				aux->next = pivote;
+				return (1);
+			}
+		}
+		aux = aux->next;
+		iterator++;
+	}
+	return (-1);
 }

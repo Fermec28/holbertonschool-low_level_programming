dd246705cde21f7322accbbace79ce6fabd5c362
Jose Medina <fermed28@gmail.com>
2019-03-28 21:40:17 +0000

    covert string binary to unsigned integer

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,22 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint -
+ * binary_to_uint - binary converter
  *
- * Return: 
+ * Return: unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int i = 0, number = 0;
 
+	while(b[i])
+	{
+		if (b[i] != '0' &&  b[i] != '1')
+			return (0);
+		number = number << 1;
+		number = number | b[i] - '0';
+		i++;
+	}
+
+	return (number);
 }

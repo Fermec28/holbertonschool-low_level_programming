9562cc7b44149cdbf0e2c8b659a8ccba55ed10a3
Jose Medina <fermed28@gmail.com>
2019-03-28 21:51:48 +0000

    add parentesys and fixes betty

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -2,19 +2,19 @@
 
 /**
  * binary_to_uint - binary converter
- *
+ * @b: binary number in string
  * Return: unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0, number = 0;
 
-	while(b[i])
+	while (b[i])
 	{
 		if (b[i] != '0' &&  b[i] != '1')
 			return (0);
 		number = number << 1;
-		number = number | b[i] - '0';
+		number = number | (b[i] - '0');
 		i++;
 	}
 

b1d2c032dc2161790a09329389c43c42c133d747
Jose Medina <fermed28@gmail.com>
2019-03-28 21:58:56 +0000

    add condition before to go trougth string

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -9,9 +9,11 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0, number = 0;
 
+	if (b == NULL)
+		return (0);
 	while (b[i])
 	{
-		if (b == NULL || (b[i] != '0' &&  b[i] != '1'))
+		if (b[i] != '0' &&  b[i] != '1')
 			return (0);
 		number = number << 1;
 		number = number | (b[i] - '0');

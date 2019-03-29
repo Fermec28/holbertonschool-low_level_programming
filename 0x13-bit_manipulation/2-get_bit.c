2b78f9b7eb1080d574f424d7cbd451714d0d81a4
Jose Medina <fermed28@gmail.com>
2019-03-29 03:56:04 +0000

    print specific bit

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
 
 /**
- * get_bit -
- *
- * Return: 
+ * get_bit - get an specific bit
+ * @n : number to extract the bit
+ * @index: index to extract
+ * Return: the index bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-
+	if (index > sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	else
+	{
+		return ( (n >> index) & 1);
+	}
 }

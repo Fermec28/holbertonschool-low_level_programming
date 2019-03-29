1b1763bb9ea856c203f608e55b30dac28cc2a3c8
Jose Medina <fermed28@gmail.com>
2019-03-29 04:12:03 +0000

    set bit in index

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 
 /**
- * set_bit -
- *
- * Return: 
+ * set_bit - set bit in index
+ * @n: number
+ * @index: index to set
+ * Return: the number
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-
+	if (index > (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	else
+	{
+		*n = *n | (1 << index);
+		return (0);
+	}
 }

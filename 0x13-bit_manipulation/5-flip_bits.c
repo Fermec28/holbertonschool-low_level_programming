95cdb0b3d2a2c3a65c93e57398f42674ecd3b395
Jose Medina <fermed28@gmail.com>
2019-03-29 05:24:00 +0000

    find bits differences

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,21 @@
 #include "holberton.h"
 
 /**
- * flip_bits -
- *
- * Return: 
+ * flip_bits - bits to flip
+ * @n: number1
+ * @m: number 2
+ * Return: number of bits;
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned int counter = 0;
 
+	n = (n ^ m);
+	while(n)
+	{
+		if((n & 1) == 1)
+			counter++;
+		n = n >> 1;
+	}
+	return (counter);
 }

ae476b68b59aea395e61aa397f4d9aabbc32e5cd
Jose Medina <fermed28@gmail.com>
2019-03-29 03:36:43 +0000

    print numbers

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 
 /**
- * print_binary -
- *
- * Return: 
+ * print_binary - print number in binary
+ * @n: number to print
  */
 void print_binary(unsigned long int n)
 {
+	if (n == 0)
+		printf("0");
+	else
+	{
+		unsigned long int aux;
 
+		aux = n >> 1;
+		if (aux)
+			print_binary(aux);
+		printf("%lu", n & 1);
+	}
 }

5a0ee166afba237bcd8b1c9f77210881301fc965
Jose Medina <fermed28@gmail.com>
2019-03-29 05:37:47 +0000

    calculate Endiannes

---
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * get_endianness - find endianness
+ * Return: 0 big - 1 little
+ */
+int get_endianness(void)
+{
+	unsigned int number = 1;
+	char *p = (char*)&number;
+
+	return (*p);
+}

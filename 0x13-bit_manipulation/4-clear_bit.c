14ae423c437ae4ae92c3f6551002ccf3b1efc5e6
Jose Medina <fermed28@gmail.com>
2019-03-29 04:35:30 +0000

    set 0 specific index

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 
 /**
- * clear_bit -
- *
- * Return: 
+ * clear_bit - set 0 in specific bit
+ * @n: number
+ * @index: index to set
+ * Return: 1 if was succes -1 if not
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-
+	if (index > sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	else
+	{
+		*n = *n & ~(1 << index);
+		return (1);
+	}
 }

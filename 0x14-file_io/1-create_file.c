6679894e48e1cc9642224f4ba4106c79e1875ddd
Jose Medina <fermed28@gmail.com>
2019-04-02 21:34:16 +0000

    use O_RDWR and O_TRUNC

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * strlen - calculate the len
+ * str_len - calculate the len
  * @str: string
  * Return: len of str
  */
@@ -22,13 +22,17 @@ int create_file(const char *filename, char *text_content)
 {
 	int status = 1, fd;
 
-	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 	{
 		close(fd);
 		return (-1);
 	}
-	text_content = text_content == NULL ? "" : text_content;
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	status = write(fd, text_content, str_len(text_content));
 	close(fd);
 	if (status == -1)

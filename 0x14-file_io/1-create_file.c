7cd2d731188317e01d18ef5f2503748eeeda07e5
Jose Medina <fermed28@gmail.com>
2019-04-02 21:00:49 +0000

    create file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -1,11 +1,39 @@
 #include "holberton.h"
-
 /**
- * create_file -
- *
- * Return: 
+ * strlen - calculate the len
+ * @str: string
+ * Return: len of str
+ */
+int str_len(char *str)
+{
+	int strl = 0;
+
+	while (str[strl])
+		strl++;
+	return (strl);
+}
+/**
+ * create_file - create file
+ * @filename: file name to create
+ * @text_content: string to write
+ * Return: status to create the file
  */
 int create_file(const char *filename, char *text_content)
 {
+	int status = 1, fd;
 
+	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	text_content = text_content == NULL ? "" : text_content;
+	status = write(fd, text_content, str_len(text_content));
+	close(fd);
+	if (status == -1)
+	{
+		return (-1);
+	}
+	return (1);
 }

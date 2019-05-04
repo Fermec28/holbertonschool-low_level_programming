86529301bda9f646f872301eb31afa709f3f9232
Jose Medina <fermed28@gmail.com>
2019-05-04 20:34:45 +0000

    number of elements

--- 0x16-doubly_linked_lists/1-dlistint_len.c
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * dlistint_len -
- *
- * Return: 
+ * dlistint_len - calculate length list
+ * @h: head list
+ * Return: length list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t number = 0;
 
+	while (h)
+	{
+		h = h->next;
+		number++;
+	}
+	return (number);
 }

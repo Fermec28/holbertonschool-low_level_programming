614f8a6359f9eb30132d0704c33f2fb70ddef822
Jose Medina <fermed28@gmail.com>
2019-05-04 21:37:57 +0000

    add node at the top

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,23 @@
 #include "lists.h"
 
 /**
- * add_dnodeint -
- *
- * Return: 
+ * add_dnodeint - add node to end
+ * @head: head of the list
+ * @n: number
+ * Return: address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new_element;
 
+	new_element = malloc(sizeof(dlistint_t));
+	if (!new_element)
+	{
+		return (NULL);
+	}
+	new_element->prev = NULL;
+	new_element->next = *head;
+	new_element->n = n;
+	*head = new_element;
+	return (new_element);
 }

0baaaa917de69aa192fd7c8a6bf1fc39b7bb5b02
Jose Medina <fermed28@gmail.com>
2019-05-04 23:06:58 +0000

    sum elements

--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -
- *
- * Return: 
+ * sum_dlistint - sum values
+ * @head: head of the list
+ * Return: sum of the num
  */
 int sum_dlistint(dlistint_t *head)
 {
-
+	if (head)
+	{
+		return (head->n + sum_dlistint(head->next));
+	}
+	else
+	{
+		return (0);
+	}
 }

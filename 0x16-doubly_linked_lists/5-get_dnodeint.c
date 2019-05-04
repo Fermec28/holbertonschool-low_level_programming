40ca819a99677ae32fe7a7334e4cfc8a0c0566a8
Jose Medina <fermed28@gmail.com>
2019-05-04 23:18:42 +0000

    get secific node with index

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -
- *
- * Return: 
+ * get_dnodeint_at_index - get node in index
+ * @head: head of the list
+ * @index: idex of the node
+ * Return: node at index or Null
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	unsigned int current_index = 0;
 
+	while(head)
+	{
+		if (index == current_index)
+			return (head);
+		head = head->next;
+		current_index++;
+	}
+	return (NULL);
 }

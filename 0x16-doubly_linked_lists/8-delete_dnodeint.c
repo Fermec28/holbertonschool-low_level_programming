12b9d13ab71f380b4bef453de53114bdbe939c9c
Jose Medina <fermed28@gmail.com>
2019-05-06 03:26:08 +0000

    code compile no works

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,11 +1,15 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index -
- *
- * Return: 
+ * delete_dnodeint_at_index - delete at specific index
+ * @head: headof the list
+ * @index: node index to be deleted
+ * Return: 1 if was possible
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	(void)head;
+	(void)index;
 
+	return (1);
 }

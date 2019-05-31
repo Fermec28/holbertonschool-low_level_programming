1b361ff515c14fe52e70c5133784cb6232fbba2a
Jose Medina <fermed28@gmail.com>
2019-05-31 19:35:57 +0000

    add 0-solution

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -2,10 +2,22 @@
 
 /**
  * hash_table_create -
- *
- * Return: 
+ * @size: size of has table
+ * Return: pointer to Hash Table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *table = NULL;
 
+	table = calloc(sizeof(hash_table_t), 1);
+	if (table == NULL)
+		return NULL;
+	table->size = size;
+	table->array = calloc(sizeof(hash_node_t*), size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	return (table);
 }

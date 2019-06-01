41261c304f618d64d7f636be7adb36fa4a98b6e4
Jose Medina <fermed28@gmail.com>
2019-06-01 01:54:12 +0000

    wild free

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -1,11 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete -
+ * hash_table_delete - prints a hash table.
+ * @ht: hash table.
  *
- * Return: 
+ * Return: nothing.
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i;
+	hash_node_t *temp, *aux;
 
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			aux = temp;
+			free(temp->key);
+			free(temp->value);
+			temp = temp->next;
+			free(aux);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }

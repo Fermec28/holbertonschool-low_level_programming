aefd9485fb33f411e7873060f736c70afddd8248
Jose Medina <fermed28@gmail.com>
2019-08-27 20:46:53 +0000

    [add] binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
 #endif /* SEARCH_ALGOS_H */
